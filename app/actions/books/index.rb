337c2c7ea5f1ddac78de9e820e4253a076bd014e
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2023-07-07 09:54:41 -0300

    setup db and tests

--- app/actions/books/index.rb
+++ app/actions/books/index.rb
@@ -1,11 +1,17 @@
-# frozen_string_literal: true
-
 module Bookshelf
   module Actions
     module Books
       class Index < Bookshelf::Action
+        include Deps["persistence.rom"]
+
         def handle(*, response)
-          response.body = self.class.name
+          books = rom.relations[:books]
+            .select(:title, :author)
+            .order(:title)
+            .to_a
+
+          response.format = :json
+          response.body = books.to_json
         end
       end
     end
